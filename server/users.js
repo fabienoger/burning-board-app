000533f70ddd1d19d9f4aec617e94445f063b35a
fabienoger <fab-oger@live.fr>
2016-04-18 19:25:41 +0200

    Add users if Users collection is empty

---
+++ server/users.js
@@ -0,0 +1,26 @@
+Meteor.startup(function() {
+  // Create Super Admin and Admin
+  if (Meteor.users.find().fetch().length === 0) {
+    console.log("Creating users: ");
+    var id;
+    var users = [
+      {name: "Super Admin", email: "super@fabienoger.com", roles: ['superadmin'], language: "france", password: "super123"},
+      {name: "Admin", email: "admin@fabienoger.com", roles: ['admin'], language: "france", password: "admin123"}
+    ];
+
+    _.each(users, function (user) {
+      console.log(user);
+
+      id = Accounts.createUser({
+        email: user.email,
+        password: user.password,
+        profile: {
+          name: user.name,
+          language: user.language
+        }
+      });
+      // Email verification
+      Meteor.users.update({_id: id}, {$set: {'emails.0.verified': true}});
+    });
+  }
+});
