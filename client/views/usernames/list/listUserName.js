59b9021a78a7ce0d1af34f6aeb21cbcdf4da6bcc
fabienoger <fab-oger@live.fr>
2016-04-20 23:29:32 +0200

    Add restriction : only admin can remove username

--- client/views/usernames/list/listUserName.js
+++ client/views/usernames/list/listUserName.js
@@ -7,15 +7,20 @@ Template.listUserNames.events({
   'click .remove-username': function(e, t) {
     var id = e.target.dataset.id;
 
-    // Call the removeUserName method
-    Meteor.call("removeUserName", id, function(err, result) {
-      if (err) {
-        console.error("removeUserName ", err);
-        Modules.client.utils.displayPanel("username-info", "negative", "warning", "Oups ! Something went wrong.");
-      } else {
-        Modules.client.utils.displayPanel("username-info", "positive", "checkmark", "Username was successfully removed !");
-      }
-    });
+    // Check if currentUser is admin
+    if (Meteor.user().profile.admin) {
+      // Call the removeUserName method
+      Meteor.call("removeUserName", id, function(err, result) {
+        if (err) {
+          console.error("removeUserName ", err);
+          Modules.client.utils.displayPanel("username-info", "negative", "warning", "Oups ! Something went wrong.");
+        } else {
+          Modules.client.utils.displayPanel("username-info", "positive", "checkmark", "Username was successfully removed !");
+        }
+      });
+    } else {
+      Modules.client.utils.displayPanel("username-info", "negative", "heart", "Ah bah non t'es pas admin ! #withLove #Peace");
+    }
   }
 });
 
