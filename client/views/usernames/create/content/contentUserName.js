29c4193c55ca40e8fb284e69fb295c8abb046324
fabienoger <fab-oger@live.fr>
2016-04-25 14:55:48 +0200

    Fix duplicates userNames

--- client/views/usernames/create/content/contentUserName.js
+++ client/views/usernames/create/content/contentUserName.js
@@ -7,30 +7,33 @@ Template.contentCreateUserName.events({
   'submit #create-username-form': function(e, t) {
     e.preventDefault();
     var userNameInput = t.find("#username-input");
+    var checkUserName = Usernames.find({"userName": userNameInput.value.trim()}).fetch();
 
-    if (userNameInput.value.trim()) {
-      var userName = {
-        userName: userNameInput.value.trim(),
-        createdAt: new Date(),
-        createdBy: Meteor.userId(),
-        taken: false
-      };
-      Meteor.call("createRandomUserName", userName, function(err, userNameId) {
-        if (err) {
-          console.error("createRandomUserName() ", err);
-          Modules.client.utils.displayPanel("username-info", "negative", "warning", "Oups ! Something went wrong !");
-        } else {
-          Modules.client.utils.displayPanel("username-info", "positive", "checkmark", "Username was successfully created !");
-          // Close modal
-//          $('.create-username.ui.small.modal').modal('hide');
-          // Empty userNameInput value
-          userNameInput.value = "";
-        }
-      });
+    // Check if userName exist
+    if (checkUserName.length <= 0) {
+      if (userNameInput.value.trim()) {
+        var userName = {
+          userName: userNameInput.value.trim(),
+          createdAt: new Date(),
+          createdBy: Meteor.userId(),
+          taken: false
+        };
+        Meteor.call("createRandomUserName", userName, function(err, userNameId) {
+          if (err) {
+            console.error("createRandomUserName() ", err);
+            Modules.client.utils.displayPanel("create-username-info", "negative", "warning", "Oups ! Something went wrong !");
+          } else {
+            Modules.client.utils.displayPanel("create-username-info", "positive", "checkmark", "Username was successfully created !");
+            // Empty userNameInput value
+            userNameInput.value = "";
+          }
+        });
+      } else {
+        Modules.client.utils.displayPanel("create-username-info", "negative", "warning", "Username field is required !");
+      }
     } else {
-      Modules.client.utils.displayPanel("username-info", "negative", "warning", "Username field is required !");
-      // Close modal
-//      $('.create-username.ui.small.modal').modal('hide');
+      console.log("UserName already exist !");
+      Modules.client.utils.displayPanel("create-username-info", "negative", "meh", "Username already exist !");
     }
   }
 });
