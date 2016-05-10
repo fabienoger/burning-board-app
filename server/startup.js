6c6ee4865f1b442958a5d3cb307d640ec4cede7c
fabienoger <fab-oger@live.fr>
2016-05-10 20:27:26 +0200

    Add new user on members of channel #general

--- server/startup.js
+++ server/startup.js
@@ -123,6 +123,13 @@ Accounts.onCreateUser(function(options, user) {
   options.profile.changeUserName = false;
   options.profile.active = true;
 
+  // Get channelGeneral object
+  var channelGeneral = Channels.findOne({name: "general"});
+  console.log("options ", options);
+  console.log("user ", user);
+  console.log("channelGeneral ", channelGeneral);
+
+
   // We still want the default hook's 'profile' behavior.
   if (options.profile) {
     user.profile = options.profile;
@@ -133,6 +140,23 @@ Accounts.onCreateUser(function(options, user) {
           console.error("upsertUserName", err);
         } else {
           console.log("Update userName succesfully !");
+        }
+      });
+    }
+
+    // Check if channelGeneral is not empty
+    if (channelGeneral) {
+      var doc = {
+        $addToSet: {
+          members: user._id
+        }
+      };
+      // Add new user to channel #general
+      Meteor.call("upsertChannel", channelGeneral._id, doc, function(err, result) {
+        if (err) {
+          console.error("upsertChannel", err);
+        } else {
+          console.log(result);
         }
       });
     }
