6fb33bbcae35e3314a1c791ef061bc8c2481f3ca
fabienoger <fab-oger@live.fr>
2016-05-30 10:46:10 +0200

    Users: Deny update profile

--- server/users/authorizations.js
+++ server/users/authorizations.js
@@ -2,12 +2,26 @@ Meteor.users.allow({
   insert: function (userId, doc) {
     return userId;
   },
+/*
   update: function (userId, doc, fields, modifier) {
     console.log("update user");
+    console.log("doc => ", doc);
+    console.log("fields => ", fields);
+    if (_.contains(fields, "admin") || _.contains(fields, "superAdmin")) {
+      console.log("update Admin or superAdmin");
+      return false;
+    }
     return false;
   },
+*/
   remove: function (userId, doc) {
     // can only remove your own documents
     return doc.userId === userId;
+  }
+});
+
+Meteor.users.deny({
+  update: function() {
+    return true;
   }
 });
