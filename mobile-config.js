d4c92684c17c6b8b4948d0282df43d5f69280617
fabienoger <fab-oger@live.fr>
2016-06-03 10:01:45 +0200

    First mobile configuration (test)

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  id: 'burningboard.fabienoger.com',
+  name: 'Burning Board',
+  description: 'Anonymous chat',
+  author: 'Fabien Oger',
+  email: 'contact@fabienoger.com',
+  website: 'http://fabienoger.com'
+});
+
+App.setPreference('orientation', 'portrait');
+
+App.accessRule('http://*');
