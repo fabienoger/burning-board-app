60c7683b4ba15d918732d6128db4a8a6ddbb9db1
fabienoger <fab-oger@live.fr>
2016-06-03 16:08:12 +0200

    On rename channel: update all message with the currentChannel name to the new channel name & secure upsertChannel method

--- server/messages/methods.js
+++ server/messages/methods.js
@@ -2,5 +2,19 @@ Meteor.methods({
   // Create message
   insertMessage: function(message) {
     return Messages.insert(message);
+  },
+  // Upsert message
+  upsertMessage: function(selector, doc, options) {
+    // Check if params are not null
+    if (selector && doc) {
+      console.log("options > ", options);
+      // Check if option is not empty
+      if (options) {
+        return Messages.upsert(selector, doc, options);
+      }
+    return Messages.upsert(selector, doc);
+    }
+    throw new Meteor.Error("missing-params", "Params 'selector' or 'doc' is missing.");
+    return;
   }
 });
